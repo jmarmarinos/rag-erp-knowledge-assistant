80cc3e3bd4210adf9218652585ca6e87236696ef
jmarmarinos <jmarmarinos@gmail.com>
2026-03-05 13:33:08 +0200

    Update test_ingestion.py

--- scripts/test_ingestion.py
+++ scripts/test_ingestion.py
@@ -1,1 +1,17 @@
+from app.vectorstores import load_vectorstore
+from app.config import CUSTOM_DOCS_DIR
+from app.config import OFFICIAL_DOCS_DIR
 
+
+
+#load_vectorstore(CUSTOM_DOCS_DIR, "blueprint" )
+
+custom_docs = load_vectorstore(CUSTOM_DOCS_DIR, "blueprint")
+
+for d in custom_docs[::3]:
+    print("\n" + "="*80)
+    print("File:", d.metadata["file_name"])
+    print("Page:", d.metadata["page_1based"])
+    print("Domain:", d.metadata["domain"])
+    print("-"*80)
+    print(d.page_content)
